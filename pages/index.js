a28eb2bccb7ba744a980ebdbdd889c423e805131
Gabriel Mendes - <107814944+GabrielProzin@users.noreply.github.com>
2024-08-08 14:29:49 +0000

    adiciona emoji de risada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Eu te amo minha princesinha, se você me ama, da uma risadinha</h1>;
+  return <h1>Eu te amo minha princesinha, se você me ama, da uma risadinha 😂</h1>;
 }
 
 export default Home;
