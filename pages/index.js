858082e0fda1d1794d8ecb6358db09880b9fed22
Gabriel Mendes - <107814944+GabrielProzin@users.noreply.github.com>
2024-08-13 12:22:00 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>Eu te amo minha princesinha, se você me ama, da uma risadinha 😂</h1>;
+  return (
+    <h1>Eu te amo minha princesinha, se você me ama, da uma risadinha 😂</h1>
+  );
 }
 
 export default Home;
