40806486f2ed2235bbf38d30ed8f89fdd43c915e
Gabriel Mendes - Prozin <107814944+GabrielProzin@users.noreply.github.com>
2024-08-23 15:03:38 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ chave: "alunos são acima a da média" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "test" });
 }
 
 export default status;
